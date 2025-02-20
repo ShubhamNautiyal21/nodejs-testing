59081caeee1a35953eaa93d9ece43a3f09a06682
Shubham Nautiyal <nautiyal2108@gmail.com>
2025-02-20 17:11:04 +0530

    users route changes

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-	res.send("respond with a resource");
+	res.send("some random users are present");
 });
 
 module.exports = router;
